0e1e82864d06e4bc1a140f25fec2f5dcd8ce83da
Jacob <stidjaco@msu.edu>
2026-04-20 10:31:37 -0400

    Codebase and README updates for repo publication

--- SupplementaryCode/script_getLabeledImages.js
+++ SupplementaryCode/script_getLabeledImages.js
@@ -10,7 +10,7 @@
 -- Information --
 Author: Jacob Stid
 Date Created: 02-03-2025
-Date Updated: 02-04-2025
+Date Updated: 04-16-2026
 Contact: stidjaco@msu.edu (Jacob Stid)
 
 -- Notes --
@@ -19,40 +19,70 @@ We then add a 5th band that is the panel-rows burned into NAIP resoltuion imager
 We then tile these images up in a python script: script_createLabeledImages.ipynb.
 
 Currently, due to requirements for acquiring NAIP projection dynamically, this is a relatively intensive script. 
-At best, we are exporting 200 arrays at a time (using the RUN ALL GEE extension), which takes several mintutes to load the server requests. 
+Approach is to only look at a small range of arrayID's for viable exports (e.g., arrayID 0 to 2000 contains 140 arrays of interest)
+Wait for all tasks to be generated before hitting RUNALL
 */
 
 // Call GM-SEUS shapefile (getPanels)
-var arrays = ee.FeatureCollection('projects/ee-stidjaco/assets/BigPanel/v1_0/GMSEUS_Arrays_Final4326');
-var panels = ee.FeatureCollection('projects/ee-stidjaco/assets/BigPanel/v1_0/GMSEUS_Panels_Final4326');
+var arrays = ee.FeatureCollection('projects/ee-stidjaco/assets/BigPanel/GMSEUS_Arrays_Final_2025_v2_0');
+var panels = ee.FeatureCollection('projects/ee-stidjaco/assets/BigPanel/GMSEUS_Panels_Final_2025_v2_0');
 
-// Due to georectification differences in NAIP and satellite/aerial imagery using for OSM, we will select only panels and arrays with panels where Source = 'CCVPV' or 'gmseus'
-var panels = panels.filter(ee.Filter.inList('Source', ['CCVPV', 'gmseus'])); var naipPanelsList = panels.aggregate_array('arrayID').distinct(); 
-var arrays = arrays.filter(ee.Filter.inList('arrayID', naipPanelsList));
-var arrays = arrays.filter(ee.Filter.gt('numRow', 10)); 
+// Filter for export efficiency (1 to 18,980 -- v2.0)
+var minArrayID = 18000; // Finished through: 0-14000
+var stepArrayID = 2000; // Unless computation is getting too slow, no change needed. Just change minArrayID. 
 
-// Filter for export efficiency
-var minArrayID = 4600;
-var maxArrayID = ee.Number(minArrayID).add(200).getInfo(); //var maxArrayID = 1000;
-var arrays = arrays.filter(ee.Filter.gte('arrayID', minArrayID)).filter(ee.Filter.lt('arrayID', maxArrayID));
+// Set date for export folder
+var date = '041626';
 
 // Set variables
-var tileBuffer = 100; // 80m buffer ensures that all arrays achieve at least one tile worth of labeled imagery 
+var seed = 15; // Set seed
 var NAIPstart = '2019'; // really this is 2021, but there is a single array in the dataset where the most recent imagery is 2019 
 var NAIPend = '2023'; // Set most recent NAIP year range (two years since NAIP is acquired at state level every two years)
-var seed = 15; // Set seed
 var scale = 0.6; // set as max of most recent imagery (0.3 to 0.6 as of 2024) 
+var tileSize = 256; // 256x256
+var naipErrorMargin = ee.ErrorMargin(0.1); // Set a geometrical operation error margin for NAIP imagery 
 
-// Set a geometrical operation error margin for NAIP imagery 
-var naipErrorMargin = ee.ErrorMargin(0.1);
+// We want this dataset to be internally consistent with NAIP imagery. 
+// Thus, due to georectification differences in NAIP and satellite/aerial imagery using for OSM, we will restrict panel-row sources to those derived by or from NAIP
+// We also want to identify a pattern (for recognition). Thus, we require a minimum number of panel-rows to create a tile
+var naipPanelRowSources = ['CCVPV', 'GMSEUS_NAIP_v1_0', 'GMSEUS_NAIP_v2_0', 'GMSEUSdigArraysPanels_v2_0']; 
+var minNumRow = 10;
+
+// Get geeUtilsStid.js functions
+var geeUtilsStid = require('users/stidjaco/SourceCode:geeUtilsStid.js');
+var getMostRecentImageNAIP = geeUtilsStid.getMostRecentImageNAIP;
+
+/*
+// TESTING & DEBUGGING
+var feature = ee.Feature(arrays.filter(ee.Filter.eq('arrayID', 4123)).first()); Map.centerObject(feature); 
+var aoi = feature.geometry().buffer(tileBuffer).bounds();
+var tileImage = getMostRecentImageNAIP(NAIP.filterBounds(aoi), true);
+var panelsArrayAndAdjacent = panels.filterBounds(aoi);  //var panelsArray = panels.filter(ee.Filter.eq('arrayID', arrayID));
+var tileMask = panelsArrayAndAdjacent.reduceToImage({properties: ['arrayID'], reducer: ee.Reducer.first()}).gt(0).unmask(0).rename('mask'); // Convert to binary (1 for panels, 0 otherwise)
+var finalImage = tileImage.addBands(tileMask).toInt().setDefaultProjection({crs: tileImage.get('origCRS'), crsTransform: tileImage.get('origTransform')});
+Map.addLayer(finalImage.select('mask'), {min: 0, max: 1})
+fdsa
+*/
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Setup
+
+// Set max arrayID to filter by
+var maxArrayID = ee.Number(minArrayID).add(stepArrayID).getInfo(); 
+
+// Based on desired tile size, set a minimum tile buffer. 
+var tileBuffer = ee.Number(tileSize).multiply(scale).divide(2) // pixel buffer in each direciton
+  .divide(100).round().multiply(100).getInfo(); // round to nearest 100's place for buffering -- print("Tile Buffer Distance for Desired Tile Size: ", tileBuffer)
+
+// Filter panels and arrays
+var panels = panels.filter(ee.Filter.inList('Source', naipPanelRowSources)); var naipPanelsList = panels.aggregate_array('arrayID').distinct(); // Select for high-fidelity NAIP delineated panel-rows
+var arrays = arrays.filter(ee.Filter.inList('arrayID', naipPanelsList)); // Get only arrays associated with high-fidelity panel-rows
+var arrays = arrays.filter(ee.Filter.gt('numRow', ee.Number(minNumRow))); // Set minimun panel-row threshold (goal is to define a pattern)
+print("Total number of candidate arrays: ", arrays.size()); 
+var arrays = arrays.filter(ee.Filter.gte('arrayID', minArrayID)).filter(ee.Filter.lt('arrayID', maxArrayID)); // Subset arrays for export -- computationally expensive, avoid memory issues
+print("Number of arrays exporting: ", arrays.size()); 
 
 // Call NAIP
 var NAIP = ee.ImageCollection('USDA/NAIP/DOQQ').filterDate(ee.DateRange(NAIPstart+'-01-01', NAIPend+'-12-31'));
-
-// // TESTING & DEBUGGING -- Important: 2886
-// var feature = ee.Feature(arrays.filter(ee.Filter.eq('arrayID', 4)).first()); Map.centerObject(feature); 
-// var tileImage = NAIP.filterDate(2022+'-01-01', 2023+'-12-31').filterBounds(feature.geometry().buffer(250).bounds()).first()
-// Map.addLayer(tileImage, {}, "Original NAIP"); print(tileImage.projection())
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Create a function to export NAIP img and mask for array
 
@@ -67,45 +97,18 @@ var getLabeledImages = function(arrayID){
   var feature = ee.Feature(arrays.filter(ee.Filter.eq('arrayID', arrayID)).first());
   var aoi = feature.geometry().buffer(tileBuffer).bounds();
   
-  // Function to get the most recent image per location without reprojection
-  var getMostRecentImageNAIP = function(collection) {
-    // Get band names
-    var bandNames = collection.first().bandNames();
-    // Get NAIP id's. These are in the form: 'm_2909522_se_15_060_20201130'
-    var longIDs = collection.aggregate_array('system:index').distinct();
-    // For each id in uniqueIDs, drop the last 9 characters "_20201130"
-    var uniqueIDs = longIDs.map(function(id){return ee.String(id).slice(0, -9);});
-    // Within each non-dated index, get the most recent image
-    var recentImages = uniqueIDs.map(function(id) {
-      var imgs = collection.filter(ee.Filter.stringContains('system:index', id));
-      var img = imgs.limit(1, 'system:time_start', false).first();  // Limit to first image when sorted by date // .sort('system:time_start', false) // Sort descending to get the most recent
-      return ee.Image(img.copyProperties(imgs.first(), imgs.first().propertyNames()));
-    });
-    // Get the image collection 
-    var imgColRecent = ee.ImageCollection(recentImages).select(bandNames);
-    
-    // Get projection info
-    var origProj = imgColRecent.first().projection().getInfo(); // NAIP.filterBounds(aoi).limit(1, 'system:time_start', false).first().projection().atScale(scale).getInfo()
-    var origCRS = origProj.crs;
-    var origTransform = origProj.transform;
-    
-    // Return a non-mosaiced image that retains native resolution (mosaicing reduces resolution and reprojects to WGS84)
-    return imgColRecent.median().set({origCRS: origCRS, origTransform: origTransform}); //.reproject({crs: origCRS, crsTransform: origTransform});
-  };
-  
   // Apply function to get most recent images per aoi
-  var tileImage = getMostRecentImageNAIP(NAIP.filterBounds(aoi));
+  var tileImage = getMostRecentImageNAIP(NAIP.filterBounds(aoi), true);
   
   //###########################\\
   //// Burn Panels into NAIP \\\\
   //###########################\\
   
-  // Get panels for array
-  var panelsArray = panels.filter(ee.Filter.eq('arrayID', arrayID));
+  // Get panels for array (and any adjacent panels, incase theres an edge case where another array is included in the tile)
+  var panelsArrayAndAdjacent = panels.filterBounds(aoi);  //var panelsArray = panels.filter(ee.Filter.eq('arrayID', arrayID));
   
   // Rasterize panel-row boundaries and create a binary NAIP image (0: no solar, 1: solar)
-  var tileMask = panelsArray.filterBounds(aoi).reduceToImage({properties: ['arrayID'], reducer: ee.Reducer.first()}).gt(0)  // Convert to binary (1 for panels, 0 otherwise)
-    .unmask(0).rename('mask')//.reproject({crs: 'EPSG:26920', scale: scale});
+  var tileMask = panelsArrayAndAdjacent.reduceToImage({properties: ['arrayID'], reducer: ee.Reducer.first()}).gt(0).unmask(0).rename('mask'); // Convert to binary (1 for panels, 0 otherwise)
     
   // Add band to to NAIP tiled image 
   var finalImage = tileImage.addBands(tileMask).toInt().setDefaultProjection({crs: tileImage.get('origCRS'), crsTransform: tileImage.get('origTransform')});
@@ -115,7 +118,7 @@ var getLabeledImages = function(arrayID){
   //######################################################\\
   
   // Prepare whole image export
-  var outFolder = "labelImgsWhole";
+  var outFolder = "labelImgsWhole_" + date;
   var fileName = 'id' + arrayID;
   
   // Export. Omit CRS and CRS transform to allow default projection to be maintained.
@@ -138,31 +141,3 @@ idList.evaluate(function(id){
     getLabeledImages(arrayID);
   });
 });
-
-// ~~~~~~~~~~~~~~~~~~~~~~ Archive
-
-  // // Set vars
-  // var imageFolder = 'trainImages';
-  // var maskFolder = 'trainMasks';
-  // var fileName = 'id' + arrayID;
-  // // Get projection info
-  // var outCRS = finalImage.get('origCRS').getInfo(); // var outCRS = finalImage.projection().crs
-  // var outTransform = finalImage.get('origTransform').getInfo();// var outTransform = finalImage.projection.transform
-  // // Export
-  // Export.image.toDrive({
-  //   image: finalImage.select("R", "G", "B", "N"),
-  //   description: fileName,
-  //   dimensions: "256x256",
-  //   folder: imageFolder, 
-  //   crs: outCRS, 
-  //   crsTransform: outTransform,
-  //   region: tile, fileFormat: 'GeoTIFF', maxPixels: 1e13});
-  // Export.image.toDrive({
-  //   image: finalImage.select("mask"),
-  //   description: fileName,
-  //   dimensions: "256x256",
-  //   folder: maskFolder,
-  //   crs: outCRS, 
-  //   crsTransform: outTransform,
-  //   region: tile, fileFormat: 'GeoTIFF', maxPixels: 1e13});
-  // return finalImage
